6b1ea971f1cc01f3747e378de028850e7fde9f41
Harold Torres Marino <haroldtm55@gmail.com>
2021-11-19 22:24:55 +1100

    Lab completed

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,5 +1,49 @@
-import React, { Component } from 'react';
+import React, { useState, Component } from "react"
 import { connect } from 'react-redux'
+
+// function UserInput({dispatch}) {
+//   const [userForm, setUserForm] = useState({
+//     username: '',
+//     hometown: ''
+//   })
+
+//   let handleInputChange =(event)=> {
+//     setUserForm({
+//       [event.target.id]: event.target.value
+//     });
+//   }
+
+//   let handleOnSubmit=(event)=> {
+//     event.preventDefault();
+//     dispatch({ type: 'ADD_USER', user: userForm });
+//   }
+
+  
+//   return(
+//     <form onSubmit={handleOnSubmit}>
+//       <p>
+//         <input
+//           type="text"
+//           id="username"
+//           onChange={handleInputChange}
+//           placeholder="username"
+//         />
+//       </p>
+//       <p>
+//         <input
+//           type="text"
+//           id="hometown"
+//           onChange={handleInputChange}
+//           placeholder="hometown"
+//         />
+//       </p>
+//       <input type="submit" />
+//     </form>
+//   )
+  
+// }
+
+// export default connect()(UserInput);
 
 class UserInput extends Component {
 
